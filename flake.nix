841ae1be77f5be6b7e10593b95c7d522a5d93a45
Joshua Gilman <joshuagilman@gmail.com>
2024-02-04 10:07:27 -0800

    chore: adds flake and updates deps

---
+++ flake.nix
@@ -0,0 +1,43 @@
+{
+  description = "actions-generate-checksum";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    devenv.url = "github:cachix/devenv";
+    mk-shell-bin.url = "github:rrbutani/nix-mk-shell-bin";
+  };
+
+  nixConfig = {
+    extra-trusted-public-keys = "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw=";
+    extra-substituters = "https://devenv.cachix.org";
+  };
+
+  outputs = inputs @ {flake-parts, ...}:
+    flake-parts.lib.mkFlake {inherit inputs;} {
+      imports = [
+        inputs.devenv.flakeModule
+      ];
+      systems = ["x86_64-linux" "i686-linux" "x86_64-darwin" "aarch64-linux" "aarch64-darwin"];
+
+      perSystem = {
+        config,
+        self',
+        inputs',
+        pkgs,
+        system,
+        ...
+      }: {
+        devenv.shells.default = {
+          name = "actions-generate-checksum";
+          packages = with pkgs; [
+            poetry
+            python312
+            ruff
+          ];
+          enterShell = ''
+            echo "Welcome to the actions-generate-checksum development environment!"
+          '';
+        };
+      };
+    };
+}
